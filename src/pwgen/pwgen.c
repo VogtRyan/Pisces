125bd5c8a32f4fedb70d94dd27de191c3c9b7d41
Ryan Vogt <52265929+VogtRyan@users.noreply.github.com>
2025-05-12 11:46:03 -0700

    Add -d option to pwgen

--- src/pwgen/pwgen.c
+++ src/pwgen/pwgen.c
@@ -23,6 +23,7 @@
 #include "common/scrub.h"
 
 #include <errno.h>
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,12 +50,40 @@ typedef enum {
 static void generate_password(char *password, size_t length, gen_fn_t genFn);
 
 /*
+ * Print a description of the generation function, including how many bits of
+ * security it provides.
+ */
+static void describe_gen_fn(size_t length, gen_fn_t genFn);
+
+/*
+ * Print a description of the generation function, outputting the number of
+ * bits of security truncated to three decimal places (so the value is never
+ * over-reported).
+ */
+static void print_description_decimal(const char *name, size_t length,
+                                      double bitsSecurity);
+
+/*
+ * Prints a description of the generation function, outputting the number of
+ * bits of security as a whole number.
+ */
+static void print_description_whole(const char *name, size_t length,
+                                    size_t bitsSecurity);
+
+/*
+ * Prints a description of the generation function, up to but not including the
+ * number of bits of security.
+ */
+static void print_description(const char *name, size_t length);
+
+/*
  * Parse the options on the command line, setting the output length and the
- * generation function. On any error, print a message to stderr and exit with
- * a negative code.
+ * generation function. If the password-generation function should be describe
+ * instead of run, raise the describe flag. On any error, print a message to
+ * stderr and exit with a negative code.
  */
 static void parse_command_line(int argc, char **argv, gen_fn_t *genFn,
-                               size_t *outputLen);
+                               size_t *outputLen, int *describe);
 
 /*
  * If no generation function has yet been set, set the function to newGenFn and
@@ -88,11 +117,12 @@ int main(int argc, char **argv)
 {
     gen_fn_t genFn;
     size_t length;
+    int describe;
     char *password = NULL;
     int errVal = 0;
 
     /* Parse the command line and ensure option sanity */
-    parse_command_line(argc, argv, &genFn, &length);
+    parse_command_line(argc, argv, &genFn, &length, &describe);
     if (genFn == GEN_FN_USQ_ENFORCED && length < 4) {
         ERROR(isErr, errVal,
               "An \"enforced\" password must have at least four characters");
@@ -103,9 +133,14 @@ int main(int argc, char **argv)
     password = (char *)calloc(length + 1, sizeof(char));
     GUARD_ALLOC(password);
 
-    /* Generate the password and output it */
-    generate_password(password, length, genFn);
-    printf("%s\n", password);
+    /* Generate and output the password, or describe the generation function */
+    if (describe) {
+        describe_gen_fn(length, genFn);
+    }
+    else {
+        generate_password(password, length, genFn);
+        printf("%s\n", password);
+    }
 
 isErr:
     if (password != NULL) {
@@ -141,16 +176,75 @@ static void generate_password(char *password, size_t length, gen_fn_t genFn)
     }
 }
 
+static void describe_gen_fn(size_t length, gen_fn_t genFn)
+{
+    switch (genFn) {
+    case GEN_FN_ALPHA_NUM:
+        print_description_decimal("Alphanumeric", length,
+                                  bits_security_alpha_num(length));
+        break;
+    case GEN_FN_ASCII:
+        print_description_decimal("ASCII", length,
+                                  bits_security_ascii(length));
+        break;
+    case GEN_FN_HEX_LOWER:
+        print_description_whole("Hexadecimal (lowercase)", length,
+                                bits_security_hex(length));
+        break;
+    case GEN_FN_HEX_UPPER:
+        print_description_whole("Hexadecimal (uppercase)", length,
+                                bits_security_hex(length));
+        break;
+    case GEN_FN_USQ_ENFORCED:
+        print_description_decimal("Enforced (uppercase, lowercase, number, "
+                                  "and U.S. QWERTY simple symbol)",
+                                  length,
+                                  bits_security_usq_simple_enforced(length));
+        break;
+    case GEN_FN_USQ_SIMPLE:
+        print_description_decimal("U.S. QWERTY with simple symbols", length,
+                                  bits_security_usq_simple(length));
+        break;
+    default:
+        ASSERT_NEVER_REACH("Invalid password-generation function");
+    }
+}
+
+static void print_description_decimal(const char *name, size_t length,
+                                      double bitsSecurity)
+{
+    double trunc;
+
+    print_description(name, length);
+    trunc = floor(bitsSecurity * 1000) / 1000;
+    printf("%.3lf\n", trunc);
+}
+
+static void print_description_whole(const char *name, size_t length,
+                                    size_t bitsSecurity)
+{
+    print_description(name, length);
+    printf("%zu\n", bitsSecurity);
+}
+
+static void print_description(const char *name, size_t length)
+{
+    printf("Method: %s\n", name);
+    printf("Length: %zu\n", length);
+    printf("Bits of security: ");
+}
+
 static void parse_command_line(int argc, char **argv, gen_fn_t *genFn,
-                               size_t *outputLen)
+                               size_t *outputLen, int *describe)
 {
     int genFnSet = 0;
     int ch;
 
     *genFn = DEFAULT_GEN_FN;
     *outputLen = 0;
+    *describe = 0;
 
-    while ((ch = getopt(argc, argv, "aeHhnsl:v")) != -1) {
+    while ((ch = getopt(argc, argv, "adeHhnsl:v")) != -1) {
         switch (ch) {
         case 'a':
             set_generation_fn(genFn, &genFnSet, GEN_FN_ASCII);
@@ -172,6 +266,9 @@ static void parse_command_line(int argc, char **argv, gen_fn_t *genFn,
             break;
         case 'l':
             set_length_value(outputLen, optarg);
+            break;
+        case 'd':
+            *describe = 1;
             break;
         default:
             usage();
@@ -240,7 +337,7 @@ isErr:
 
 static void usage(void)
 {
-    fprintf(stderr, "usage: pwgen [-aeHhns] [-l length]\n");
+    fprintf(stderr, "usage: pwgen [-adeHhns] [-l length]\n");
     fprintf(stderr, "pwgen version %s\n", IMPLEMENTATION_VERSION);
     exit(-1);
 }
